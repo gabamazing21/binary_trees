1f75cf30faa0efd1c75c35465ac07c5846cc6093
Gabriel Daramola <gabamazing21@gmail.com>
2024-02-01 06:40:46 +0100

    okay

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -21,6 +21,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 		parent->right = newNode;
 		parent->right->right = temp;
+		if (temp != NULL)
+			temp->parent = newNode;
 	}
 	return (newNode);
 }
