6f3cc05f18d7e5140696bf2a7d40578b1d2644af
Gabriel Daramola <gabamazing21@gmail.com>
2024-02-01 04:34:29 +0100

    task 2 completed

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - insert right
+ * @parent: parent node
+ * @value: value
+ * Return: return new node
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode = binary_tree_node(parent, value);
+
+	if (parent->right == NULL)
+	{
+		parent->right = newNode;
+	}
+	else
+	{
+		binary_tree_t *temp = parent->right;
+
+		parent->right = newNode;
+		parent->right->right = temp;
+	}
+	return (newNode);
+}
