b626503f2069e3d1b18f21d84b846a0fab6e0800
Gabriel Daramola <gabamazing21@gmail.com>
2024-02-01 19:08:40 +0100

    klj

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,9 +8,8 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	
 	binary_tree_t *newNode = binary_tree_node(parent, value);
-	
+
 	if (parent == NULL)
 		return (NULL);
 	if (parent->left == NULL)
