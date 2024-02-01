f97224e1b86f3be6430dbff60bcf5552caf744e6
Gabriel Daramola <gabamazing21@gmail.com>
2024-02-01 18:32:27 +0100

    let's see

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,10 +8,10 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newNode = binary_tree_node(parent, value);
-
 	if (parent == NULL)
 		return (NULL);
+	binary_tree_t *newNode = binary_tree_node(parent, value);
+
 	if (!newNode)
 		return (NULL);
 	if (parent->left == NULL)
