cd6d8367ac84003b30d6fe22d2bc5d6bed89cbb2
Gabriel Daramola <gabamazing21@gmail.com>
2024-02-01 16:00:31 +0100

    okay

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+* binary_tree_postorder - preorfer traversal
+* @tree: root node
+* @func: pointer to function
+*/
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+		if (tree == NULL)
+			return;
+		if (func == NULL)
+			return;
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+}
