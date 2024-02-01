88025cba2a060d26bb5de7c510eeb7c1f85b4b53
Gabriel Daramola <gabamazing21@gmail.com>
2024-02-01 03:08:36 +0100

    add null save

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,6 +9,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	/*Allocate memory for new node*/
 	binary_tree_t *node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+
+	if (node == NULL)
+		return (NULL);
 	/*Assign data to this node*/
 	node->parent = parent;
 	node->n = value;
