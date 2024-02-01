0bbb713321eb3ae920ec671f4ed06be8b06959ef
Gabriel Daramola <gabamazing21@gmail.com>
2024-02-01 20:51:32 +0100

    gotcha

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -5,13 +5,31 @@
 * @tree: node to check
 * Return: height
 */
+int height_helper(const binary_tree_t *tree);
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
+		return (0);
+	size_t result = height_helper(tree);
+
+	return (result);
+}
+/**
+* height_helper - helper function
+* @tree: node to check
+* Return: return result
+*/
+int height_helper(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
 		return (-1);
+	}
+	else
+	{
+		int left = height_helper(tree->left);
+		int right = height_helper(tree->right);
 
-	int left = binary_tree_height(tree->left);
-	int right = binary_tree_height(tree->right);
-
-	return (fmax(left, right) + 1);
+		return (fmax(left, right) + 1);
+	}
 }
