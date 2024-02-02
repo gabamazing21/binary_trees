2259033d3a82c05748526ff38925f68de789a109
Gabriel Daramola <gabamazing21@gmail.com>
2024-02-02 02:55:56 +0100

    get balance facot

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+/**
+* binary_tree_balance - balance factor
+* @tree: node to check
+* Return: balance factor
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int r, l, bf;
+
+	if (tree == NULL)
+		return (0);
+	l = (int)(binary_tree_height(tree->left));
+	r = (int)(binary_tree_height(tree->right));
+	bf = l - r;
+	return (bf);
+}
+/**
+* binary_tree_height - get the height
+* @tree: node to check
+* Return: height
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (tree)
+	{
+		left = (tree->left) ? 1 + binary_tree_height(tree->left) : 0;
+		right = (tree->right) ? 1 + binary_tree_height(tree->right) : 0;
+		return ((left > right) ? left : right);
+	}
+	return (0);
+}
