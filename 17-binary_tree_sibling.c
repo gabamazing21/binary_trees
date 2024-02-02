7f0cafb599afc3f83388301077fbc5ca8ea91335
Gabriel Daramola <gabamazing21@gmail.com>
2024-02-02 05:59:33 +0100

    ok

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,8 +10,6 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	{
 		return (NULL);
 	}
-	if (node->left == NULL && node->right == NULL)
-		return (NULL);
 	if (node->parent->left == node) {
         return node->parent->right;
     } else {
