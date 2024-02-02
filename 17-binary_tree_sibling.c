d26d7776cc2460bd22b769366b016d9f661e64db
Gabriel Daramola <gabamazing21@gmail.com>
2024-02-02 05:58:46 +0100

    ok

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_sibling - check if it's perfect
+ * @node: node to check
+ * Return: 0 or 1
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+	{
+		return (NULL);
+	}
+	if (node->left == NULL && node->right == NULL)
+		return (NULL);
+	if (node->parent->left == node) {
+        return node->parent->right;
+    } else {
+        return node->parent->left;
+    }
+}
