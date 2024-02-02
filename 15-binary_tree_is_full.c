9ad6405f67bf3f93307b7ef39634fb5f690a5adb
Gabriel Daramola <gabamazing21@gmail.com>
2024-02-02 03:50:03 +0100

    task 5

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+/**
+* binary_tree_is_full - count nodes
+* @tree: node tree
+* Return: return 0 or 1
+*/
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (1);
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	if (tree->left == NULL || tree->right == NULL)
+	{
+		return (0);
+	}
+	if (tree->left && tree->right)
+	{
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	}
+	return (0);
+}
